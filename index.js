4b6d28da1e8c1e558b235a61b6ba6fe7ad6c5435
weihang-z <weihangzeng79@gmail.com>
2025-11-12 22:05:22 -0500

    Fix session cookie domain for cross-origin authentication

--- index.js
+++ index.js
@@ -28,7 +28,6 @@ if (process.env.SERVER_ENV !== "development") {
   sessionOptions.cookie = {
     sameSite: "none",
     secure: true,
-    domain: process.env.SERVER_URL,
   };
 }
 app.use(session(sessionOptions));
